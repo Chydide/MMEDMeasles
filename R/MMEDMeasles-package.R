a36040f7232feee6fba755215818fd4c79014935
Dean Malan <delena.malan@gmail.com>
2025-06-23 17:51:49 +0200

    Fix NAMESPACE

---
+++ R/MMEDMeasles-package.R
@@ -0,0 +1,15 @@
+#' MMEDMeasles
+#'
+#' SACEMA MMED Measles model.
+#'
+#' @description SACEMA MMED Measles model
+#'
+#' @name MMEDMeasles
+#' @useDynLib MMEDMeasles, .registration = TRUE
+#'
+#' @import methods
+#' @import Rcpp
+#' @importFrom rstan sampling
+#' @importFrom rstantools rstan_config
+#' @importFrom RcppParallel RcppParallelLibs
+"_PACKAGE"
