1dd7330495ab6c51794e5cdf85bc463f393b6f70
Dean Malan <delena.malan@gmail.com>
2025-06-25 15:52:00 +0200

    Call model

--- example.R
+++ example.R
@@ -1,9 +1,16 @@
 #source("measles_stan.R")
 
 data <- list(
-  n_observations = 2,
+  n_observations = 3,
   total_population = 100,
-  vaccinated = c(20, 30)
+  vaccinated = c(20, 30, 40)
 )
 fit <- measles_stan(data, chains=1, iter = 20, refresh = 5)
+fit
+
+data <- list(
+  n_observations = 3,
+  total_population = 100
+)
+fit <- measles_stan(data, chains=1, iter = 20, refresh = 5, algorithm="Fixed_param")
 fit
