763bbcd6b013c8f22fe77027582d060fcd54ed16
Dean Malan <delena.malan@gmail.com>
2025-06-25 14:50:35 +0200

    Add new model

--- example.R
+++ example.R
@@ -8,4 +8,27 @@ data <- list(
   sample_size = array(c(c(100, 200), c(100, 200)), dim=c(2, 2)),
   population_size = array(c(c(1000, 1000), c(900, 800)), dim=c(2, 2))
 )
+data <- list(
+  n_observations = 1,
+  n_cohorts = 2,
+  age = array(c(c(0, 0)), dim=c(2, 1)),
+  vaccine_status = array(c(c(0, 0)), dim=c(2, 1)),
+  sample_size = array(c(c(100, 200)), dim=c(2, 1)),
+  population_size = array(c(c(1000, 1000)), dim=c(2, 1))
+)
 measles_stan(data, chains=1, iter = 20, refresh = 5)
+
+out <- rstan::sampling(
+  stanmodels$measles,
+  data = list(
+    n_observations = 2,
+    n_cohorts = 2,
+    age = array(, dim=c(2, 2)),
+    vaccine_status = array(c(c(10, 20), c(70, 100)), dim=c(2, 2)),
+    sample_size = array(c(c(100, 200), c(100, 200)), dim=c(2, 2)),
+    population_size = array(c(c(1000, 1000), c(900, 800)), dim=c(2, 2))
+  ),
+  iter=1,
+  chains=1,
+  algorithm="Fixed_param"
+)
---
+++ inst/stan/measles2.stan
@@ -0,0 +1,28 @@
+//
+// Learn more about model development with Stan at:
+//
+//    http://mc-stan.org/users/interfaces/rstan.html
+//    https://github.com/stan-dev/rstan/wiki/RStan-Getting-Started
+//
+
+// The input data.
+data {
+  int<lower=0> n_observations;
+  int<lower=0> total_population; # P for short
+  array[n_observations] int vaccinated;
+}
+
+// The parameters accepted by the model.
+parameters {
+  vector[n_observations] force_of_vaccination;
+}
+
+// The model to be estimated.
+model {
+  int population_left = total_population;
+  
+  for (i in 1:n_observations) {
+    vaccinated[i] ~ binomial(population_left, 1 - exp(-force_of_vaccination[i]));
+    population_left = population_left - vaccinated[i]; 
+  }
+}
